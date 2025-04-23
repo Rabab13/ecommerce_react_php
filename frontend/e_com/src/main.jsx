34401a33a619ea1fbb6fa2a0bc722d75a70b7c12
Rabab Ahmed Fouad <72532543+Rabab13@users.noreply.github.com>
2025-04-23 10:47:43 +0200

    Update main.jsx
    Switched to ngrok since Railway free tier expired and upgrade failed (bank card declined).
    ngrok's being used for now to expose the local backend.

--- frontend/e_com/src/main.jsx
+++ frontend/e_com/src/main.jsx
@@ -7,7 +7,7 @@ import App from './App';
 import './index.css';
 
 const client = new ApolloClient({
-  uri: ' https://b3d7-196-134-166-210.ngrok-free.app', // Replace with your endpoint
+  uri: 'https://a3ce-196-134-176-202.ngrok-free.app', // Replace with your endpoint
   cache: new InMemoryCache(),
 });
 
