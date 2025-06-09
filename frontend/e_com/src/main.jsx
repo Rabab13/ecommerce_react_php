07a37465323ec8bc782823210c8c2eea6897c132
Rabab Ahmed Fouad <72532543+Rabab13@users.noreply.github.com>
2025-06-09 15:38:01 +0300

    Update main.jsx

--- frontend/e_com/src/main.jsx
+++ frontend/e_com/src/main.jsx
@@ -7,7 +7,7 @@ import App from './App';
 import './index.css';
 
 const client = new ApolloClient({
-  uri: 'https://b5ce-196-134-152-36.ngrok-free.app',  //updated ngrok URL
+  uri: 'https://f8ce-196-134-174-68.ngrok-free.app',  //updated ngrok URL
   cache: new InMemoryCache(),
 });
 
