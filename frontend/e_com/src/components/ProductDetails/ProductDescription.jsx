ddfaa76270181584c7cb0b8267422850ba50db07
Rabab <rababrain@gmail.com>
2025-06-25 17:46:35 +0300

    Fix: Completed all requirements

--- frontend/e_com/src/components/ProductDetails/ProductDescription.jsx
+++ frontend/e_com/src/components/ProductDetails/ProductDescription.jsx
@@ -3,7 +3,9 @@ import parse from 'html-react-parser';
 
 const ProductDescription = ({ description }) => {
   return (
-    <div className="w-[300px] h-[200px] pt-3 md:w-72 overflow-y-auto font-roboto font-normal text-[16px] leading-[160%] text-gray-700 prose prose-sm">
+    <div className="w-[300px] h-[200px] pt-3 md:w-72 overflow-y-auto font-roboto font-normal text-[16px] leading-[160%] text-gray-700 prose prose-sm"
+      data-testid="product-description"
+    >
       {parse(description)}
     </div>
   );
