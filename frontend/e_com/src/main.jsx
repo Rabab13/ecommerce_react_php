17e3bc501d16759132172eb5ab55f5f5fba52e5b
Rabab Ahmed Fouad <72532543+Rabab13@users.noreply.github.com>
2025-07-04 11:42:24 +0300

    Update main.jsx

--- frontend/e_com/src/main.jsx
+++ frontend/e_com/src/main.jsx
@@ -18,7 +18,7 @@ import '@fontsource/source-sans-pro/600.css';
 import '@fontsource/source-sans-pro/700.css';
 
 const client = new ApolloClient({
-  uri: 'https://65e8-196-134-108-13.ngrok-free.app', 
+  uri: 'https://0133-196-134-111-172.ngrok-free.app', 
   cache: new InMemoryCache(),
 });
 
