4b19702af07cbdfb3b087d6b302e254036690ce7
Rabab Ahmed Fouad <72532543+Rabab13@users.noreply.github.com>
2025-07-08 03:20:57 +0300

    Update vite.config.js

--- frontend/e_com/vite.config.js
+++ frontend/e_com/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
 
     proxy: {
       '/graphql.php': {
-        target: 'https://e07c-196-128-130-235.ngrok-free.app',
+        target: 'https://062d144cb85a.ngrok-free.app',
         changeOrigin: true,
         secure: false,
         configure: (proxy) => {
